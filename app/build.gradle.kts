935017c4545f70a234bfbf0eb8822b029dd3b89c
SatnamSinghMaggo <satnamsinghmaggo153@gmail.com>
2025-04-08 18:22:27 +0530

    Pdf view is done and screen are working need designing now.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.barteksc:android-pdf-viewer:3.2.0-beta.1")
 
 
 }
