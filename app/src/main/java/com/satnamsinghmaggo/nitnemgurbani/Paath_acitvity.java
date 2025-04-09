327e49268f5d809f40011f482e832983dbd11f85
SatnamSinghMaggo <satnamsinghmaggo153@gmail.com>
2025-04-09 13:02:00 +0530

    Paath acitvity got pdfviewer also.

--- app/src/main/java/com/satnamsinghmaggo/nitnemgurbani/Paath_acitvity.java
+++ app/src/main/java/com/satnamsinghmaggo/nitnemgurbani/Paath_acitvity.java
@@ -11,13 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class Paath_acitvity extends AppCompatActivity {
+
+    PDFView pdfView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_paath_acitvity);
+
+        pdfView1 = findViewById(R.id.pdfview1);
+        pdfView1.fromAsset("presentation.pdf").load();
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
